cd61c9a53fbabfc05fa70693b05136bc249b48cc
krishan <krishanmondal273@gmail.com>
2024-05-14 09:58:13 +0530

    Fixed some issues

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ flipBtn.addEventListener("click", () => {
 });
 
 function updateStats() {
-  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
-  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+  document.querySelector("#heads-count").textContent = `Heads : ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails : ${tails}`;
 }
 
 function disableBtn() {
